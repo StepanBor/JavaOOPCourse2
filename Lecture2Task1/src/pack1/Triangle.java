8093d47db8318366b1bd895a0e3472f2da958bbe
StepanBor <green7@bigmir.net>
2018-04-16 23:20:48 +0300

    1

--- Lecture2Task1/src/pack1/Triangle.java
+++ Lecture2Task1/src/pack1/Triangle.java
@@ -5,22 +5,31 @@ public class Triangle extends Shape {
 	private Point p1;
 	private Point p2;
 	private Point p3;
+	private double side1;
+	private double side2;
+	private double side3;
 
 	public Triangle(Point p1, Point p2, Point p3) {
 		super();
 		this.p1 = p1;
 		this.p2 = p2;
 		this.p3 = p3;
+		side1 = this.p1.getDistToPoint(this.p2);
+		side2 = this.p1.getDistToPoint(this.p3);
+		side3 = this.p2.getDistToPoint(this.p3);
 	}
 
 	public Triangle() {
 		super();
 	}
-
-	private double side1 = p1.getDistToPoint(p2);
-	private double side2 = p1.getDistToPoint(p3);
-	private double side3 = p2.getDistToPoint(p3);
-
+	
+	public Point getCenterPoint() {
+		Point c=new Point();
+		c.setX((p1.getX()+p2.getX()+p3.getX())/3);
+		c.setY((p1.getY()+p2.getY()+p3.getY())/3);
+		return c;
+	}
+	
 	@Override
 	public double getPerimetr() {
 		return side1 + side2 + side3;
