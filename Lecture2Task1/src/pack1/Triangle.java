02134e38ccd1a6cb258fe1e0e190bffdfb4c5b37
Stepan <green7@bigmir.net>
2018-04-16 13:56:25 +0300

    5
    45

---
+++ Lecture2Task1/src/pack1/Triangle.java
@@ -0,0 +1,40 @@
+package pack1;
+
+public class Triangle extends Shape {
+
+	Point p1;
+	Point p2;
+	Point p3;
+
+	public Triangle(Point p1, Point p2, Point p3) {
+		super();
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+	}
+	
+	
+	
+	public Triangle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	@Override
+	public double getPerimetr() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double gatArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	
+	
+}
