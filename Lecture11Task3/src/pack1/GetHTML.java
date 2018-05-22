151835633e99da15905d36db562a6d2b9687d661
StepanBor <green7@bigmir.net>
2018-05-22 22:17:54 +0300

    23
    23

--- Lecture11Task3/src/pack1/GetHTML.java
+++ Lecture11Task3/src/pack1/GetHTML.java
@@ -1,7 +1,43 @@
 package pack1;
 
+import java.io.*;
+import java.net.*;
+
 public class GetHTML {
-	
-	
+
+	// private URL url;
+	//
+	// public GetHTML(URL url) {
+	// super();
+	// this.url = url;
+	// }
+	//
+	// public GetHTML() {
+	// super();
+	//
+	// }
+
+	public static File getHTML(URL url) throws IOException {
+
+		File html = new File("HTML.htm");
+		html.createNewFile();
+
+		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(html))) {
+			// try (OutputStream os = new FileOutputStream(html); InputStream is =
+			// urlConnection.getInputStream()) {
+			// byte[] buffer=new byte[1024*1024];
+			String temp;
+			
+			while ((temp=br.readLine()) != null) {
+				bw.write(temp+System.lineSeparator());
+			}
+
+		}
+
+		return html;
+
+	}
 
 }
--- Lecture11Task3/src/pack1/ReffMain.java
+++ Lecture11Task3/src/pack1/ReffMain.java
@@ -1,14 +1,20 @@
 package pack1;
 
+import java.io.*;
 import java.net.*;
 
 public class ReffMain {
 
 	public static void main(String[] args) {
 		
-		String Location;
 		
-		URL adress=new URL(Location);
+		try {
+			URL url=new URL("http://4pda.ru/");
+			File html=GetHTML.getHTML(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 	
