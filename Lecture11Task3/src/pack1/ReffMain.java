360120f3d5908d21b15d4dafcacc28d21e76fee0
Stepan <green7@bigmir.net>
2018-05-22 18:01:05 +0300

    43
    43

---
+++ Lecture11Task3/src/pack1/ReffMain.java
@@ -0,0 +1,17 @@
+package pack1;
+
+import java.net.*;
+
+public class ReffMain {
+
+	public static void main(String[] args) {
+		
+		String Location;
+		
+		URL adress=new URL(Location);
+
+	}
+	
+	
+
+}
