4f5cb4dc7e56f829d29f81e24b170f06728e3f40
Stepan <green7@bigmir.net>
2018-05-23 14:11:22 +0300

    230518
    230518

--- Lecture11Task3/src/pack1/GetHTML.java
+++ Lecture11Task3/src/pack1/GetHTML.java
@@ -5,33 +5,38 @@ import java.net.*;
 
 public class GetHTML {
 
-	// private URL url;
-	//
-	// public GetHTML(URL url) {
-	// super();
-	// this.url = url;
-	// }
-	//
-	// public GetHTML() {
-	// super();
-	//
-	// }
+	public static String getStringHTML(URL url) throws IOException {
 
-	public static File getHTML(URL url) throws IOException {
+		String temp2 = "";
+		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+
+			String temp;
+
+			while ((temp = br.readLine()) != null) {
+				temp2 += temp + System.lineSeparator();
+
+			}
+
+		}
+
+		return temp2;
+
+	}
+
+	public static File getFileHTML(URL url) throws IOException {
 
 		File html = new File("HTML.htm");
 		html.createNewFile();
 
 		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-				BufferedWriter bw = new BufferedWriter(new FileWriter(html))) {
-			// try (OutputStream os = new FileOutputStream(html); InputStream is =
-			// urlConnection.getInputStream()) {
-			// byte[] buffer=new byte[1024*1024];
-			String temp;
-			
-			while ((temp=br.readLine()) != null) {
-				bw.write(temp+System.lineSeparator());
+
+		try (OutputStream os = new FileOutputStream(html); InputStream is = urlConnection.getInputStream()) {
+			byte[] buffer = new byte[1024 * 1024];
+			int temp;
+
+			while ((temp = is.read(buffer)) != -1) {
+				os.write(buffer, 0, temp);
 			}
 
 		}
--- Lecture11Task3/src/pack1/ReffMain.java
+++ Lecture11Task3/src/pack1/ReffMain.java
@@ -3,21 +3,48 @@ package pack1;
 import java.io.*;
 import java.net.*;
 
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class ReffMain {
 
 	public static void main(String[] args) {
-		
-		
+
+		String html = "";
+
+		File linksList = new File("links.txt");
+
 		try {
-			URL url=new URL("http://4pda.ru/");
-			File html=GetHTML.getHTML(url);
+			URL url = new URL("https://www.youtube.com/");
+			html = GetHTML.getStringHTML(url);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Document htmlDoc = Jsoup.parse(html);
+
+		Elements links = htmlDoc.select("a[href]");
+
+		System.out.println(htmlDoc.title());
+
+		try (PrintWriter er = new PrintWriter(linksList)) {
+
+			for (Element element : links) {
+
+				er.println(element);
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
-	
 
 }
